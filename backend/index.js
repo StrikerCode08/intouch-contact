bca05dec5428e6f8d14a12cc763eeb8c98ae2bba
akash_bajpai_pennep <akash.bajpai@pennep.com>
2023-10-29 12:11:08 +0530

    env fix

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 //db connection
-console.log(process.env.DB_URL);
 const connectWithDb = () => {
   mongoose
     .connect(process.env.DB_URL, {
